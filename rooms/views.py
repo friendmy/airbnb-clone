01079775f65c20bce51d6b11250e67827bbf8661
friendmy <friendmytrue@gmail.com>
2020-09-15 08:36:26 +0900

    #14.4

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, View
-from django_countries import countries
 from django.core.paginator import Paginator
 from . import models, forms
 
